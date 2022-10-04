2f7a04e70c55d911bd113a9d35e78c34be7aded8
Aleks Moroz <aleksmorro@gmail.com>
2022-10-04 21:24:26 +0300

    update

---
+++ src/LaravelWpdewServiceProvider.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Wpdew\HelperPack;
+
+use Illuminate\Support\ServiceProvider;
+
+class LaravelWpdewServiceProvider extends ServiceProvider
+{
+    /**
+     * Boot the service provider.
+     */
+    public function boot()
+    {
+        $source = __DIR__.'/config/wpdew.php';
+        
+        $this->publishes([
+            $source => config_path('wpdew.php')
+        ], 'config');
+
+        //$this->mergeConfigFrom($source, 'wpdew');
+    }
+
+    /**
+     * Register the service provider.
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides()
+    {
+        return [Analytics::class, 'wpdew'];
+    }
+}
