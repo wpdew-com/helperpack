d8b9ca0a30dc9dc9e6965825c303e299cf64baf6
Aleks Moroz <aleksmorro@gmail.com>
2022-10-05 09:01:08 +0300

    update

--- src/LaravelWpdewServiceProvider.php
+++ src/LaravelWpdewServiceProvider.php
@@ -26,26 +26,7 @@ class LaravelWpdewServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        $this->app->bind(Analytics::class, function ($app) {
-            $config = $app['config'];
-
-            $analytics = new Analytics($config->get('gamp.is_ssl', false), $config->get('gamp.is_disabled', false));
-
-            $analytics->setProtocolVersion($config->get('gamp.protocol_version', 1))
-                ->setTrackingId($config->get('gamp.tracking_id'));
-
-            if ($config->get('gamp.anonymize_ip', false)) {
-                $analytics->setAnonymizeIp('1');
-            }
-
-            if ($config->get('gamp.async_requests', false)) {
-                $analytics->setAsyncRequest(true);
-            }
-
-            return $analytics;
-        });
-
-        $this->app->alias(Analytics::class, 'gamp');
+        //
     }
 
     /**
@@ -56,6 +37,5 @@ class LaravelWpdewServiceProvider extends ServiceProvider
     public function provides()
     {
         //
-        return [Analytics::class, 'gamp'];
     }
 }
