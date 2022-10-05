d401383d8f82864b12f64b45919956f25387f6cd
Aleks Moroz <aleksmorro@gmail.com>
2022-10-05 09:45:34 +0300

    update

--- src/aliases.php
+++ src/aliases.php
@@ -7,3 +7,15 @@ $classMap = [
 foreach ($classMap as $class => $alias) {
     class_alias($class, $alias);
 }
+
+/**
+ * This class needs to be defined explicitly as scripts must be recognized by
+ * the autoloader.
+ */
+
+
+if (\false) {
+    class WpdewHelp extends \Wpdew\HelperPack\HelperPack
+    {
+    }
+}
