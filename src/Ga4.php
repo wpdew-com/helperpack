fa9a20ebdf818604d13fc8eac6b636ef99ed3440
Aleks Moroz <aleksmorro@gmail.com>
2022-10-15 12:49:36 +0300

    update GA4

--- src/Ga4.php
+++ src/Ga4.php
@@ -79,4 +79,79 @@ class Ga4
 
     }
 
+    
+    function FullParamMetriks($getdata) {
+        $property_id = $_ENV['MEASUREMENT_ID'];
+        $path = $_ENV['GOOGLE_APPLICATION_CREDENTIALS']; 
+        putenv("GOOGLE_APPLICATION_CREDENTIALS=$path");
+        $client = new BetaAnalyticsDataClient();
+
+
+        $response = $client->runReport([
+            'property' => 'properties/' . $property_id,
+            'dateRanges' => [
+                new DateRange([
+                    'start_date' => $getdata['start_date'], // 'YYYY-MM-DD', today, yesterday, 7daysAgo, 14daysAgo, 30daysAgo, 90daysAgo, 365daysAgo, 1095daysAgo
+                    'end_date' => $getdata['end_date'], // 'YYYY-MM-DD', today, yesterday, 7daysAgo, 14daysAgo, 30daysAgo, 90daysAgo, 365daysAgo, 1095daysAgo
+                ]),
+            ],
+            'metrics' => [new Metric(
+                [
+                    'name' => 'activeUsers', //$getdata['metric'], // activeUsers, newUsers, sessions, totalUsers, screenPageViews
+                    //'name' => 'screenPageViews',
+                ]
+                ),new Metric(
+                [
+                    //'name' => $getdata['metric'], // activeUsers, newUsers, sessions, totalUsers, screenPageViews
+                    'name' => 'screenPageViews',
+                ]
+            )
+            ],
+            'dimensions' => [
+                //new Dimension(
+                //    [
+                //        'name' => 'browser', 
+                //    ]),
+                new Dimension(
+                    [
+                        'name' => 'date',
+                    ]),
+                //new Dimension(
+                //    [
+                //        'name' => 'deviceCategory',
+                //    ])
+            ],
+            'limit' => 100,
+        ]);
+        //$data = "";
+        //metricAggregations 
+
+
+        
+        foreach ($response->getRows() as $row) {
+            //$data = $row->getMetricValues()[0]->getValue();
+            // get browser
+       // $data = $row->getdimensionValues()[0];
+            //foreach create array and sort by date
+
+            //$row arra sort by date
+            $data[] = array(
+                'date' => $row->getDimensionValues()[0]->getValue(),
+                //'browser' => $row->getDimensionValues()[0]->getValue(),
+                //'deviceCategory' => $row->getDimensionValues()[2]->getValue(),
+                'activeUsers' => $row->getMetricValues()[0]->getValue(),
+                'screenPageViews' => $row->getMetricValues()[1]->getValue(),
+            );
+
+
+
+
+            
+        }
+
+        return $data;
+
+    }
+
+
 }
